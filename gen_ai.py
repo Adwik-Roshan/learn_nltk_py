376151d1b4c26492c18e181236a7ea117f295b82
Adwik Roshan <adwik.roshan@student.manchester.ac.uk>
2025-08-12 20:20:04 +0530

    tokens.py created, word_tokenize utilised

--- gen_ai.py
+++ gen_ai.py
@@ -1,4 +1,8 @@
 import nltk
+# nltk.download('punkt_tab')
 import numpy as np
 
+# sample_text =  "I am learning GENerative AI"
+# tokens= nltk.word_tokenize(sample_text.lower())
+# print("Tokens: ", tokens)
 print("I like Perplexity.")
